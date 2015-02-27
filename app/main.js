93168b1e5e8009b4d6426060915b531e4454a412
Vincenzo Chianese <vincenz.chianese@yahoo.it>
2015-02-27 11:51:31 +0100

    Main.js, deps and index file

---
+++ app/main.js
@@ -0,0 +1,4 @@
+import angular from 'angular';
+import "restangular";
+
+var polls = angular.module("polls", ["restangular"]);
--- config.js
+++ config.js
@@ -2,7 +2,15 @@ System.config({
   "baseURL": "/",
   "transpiler": "babel",
   "paths": {
-    "*": "*.js"
+    "*": "*.js",
+    "github:*": "jspm_packages/github/*.js"
+  }
+});
+
+System.config({
+  "map": {
+    "angular": "github:angular/bower-angular@1.3.14",
+    "restangular": "github:mgonto/restangular@1.4.0"
   }
 });
 
