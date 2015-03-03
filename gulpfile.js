00616dc8a3afbc2358c92400f3c251fc6ac1e7dc
Vincenzo Chianese <vincenz.chianese@yahoo.it>
2015-03-03 13:52:58 +0100

    gulpfile for deploy

--- gulpfile.js
+++ gulpfile.js
@@ -3,5 +3,14 @@ var git   = require("gulp-git");
 
 gulp.task("deploy",function()
 {
-  
+   git.checkout('deploy', function (err) {
+    if (err) throw err;
+     git.merge('master', function (err) {
+        if (err) throw err;
+            gulp.src('./build.*').pipe(git.add()).pipe(git.commit('Deploy'));
+       });
+    git.push('origin','deploy',function(err) {
+        if (err) throw err;
+    });
+   });
 });
