8464908e02458ef67f8c44e2240386f2be0aeef5
Vincenzo Chianese <vincenz.chianese@yahoo.it>
2015-03-20 13:15:50 +0100

    removed karma-mocha-debug

--- karma.conf.js
+++ karma.conf.js
@@ -10,7 +10,7 @@ module.exports = function (config) {
 
         // frameworks to use
         // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-        frameworks: ['systemjs', 'requirejs', 'mocha-debug', 'mocha', 'chai'],
+        frameworks: ['systemjs', 'requirejs', 'mocha', 'chai'],
 
 
         // list of files / patterns to load in the browser
