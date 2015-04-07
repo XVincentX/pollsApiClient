af447281da3e8f0319f9c517606b697dbce0f424
Vincenzo Chianese <vincenz.chianese@yahoo.it>
2015-04-07 18:12:44 +0200

    Rewrote the sample test

--- tests/unit/pollsService-test.js
+++ tests/unit/pollsService-test.js
@@ -1,5 +1,17 @@
-describe("PollsService unit tests", function () {
-    it("Should touch nasino", function () {
+import 'angular-mocks'
+import '../../app/main'
+
+describe('PollsService unit tests', function () {
+    var Restangular = null
+
+    beforeEach(angular.mock.module('pollsClient'));
+
+    beforeEach(angular.mock.inject(function (_Restangular_) {
+        Restangular = _Restangular_;
+    }));
+
+    it('Should touch nasino', function () {
+        console.log(Restangular);
         assert.isTrue(true);
     });
 });
