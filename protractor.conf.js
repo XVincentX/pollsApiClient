8e4929b9785ffd872d5283b60307321cf4d0d802
Vincenzo Chianese <vincenz.chianese@yahoo.it>
2015-04-07 13:34:02 +0200

    Switch from jasmite to mocha for e2e tests.

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,6 +1,12 @@
 exports.config = {
     seleniumAddress: 'http://localhost:4444/wd/hub',
     seleniumServerJar: './node_modules/protractor/selenium/selenium-server-standalone-2.45.0.jar',
-    specs: ['tests/e2e/*.js'],
-    baseUrl: 'http://localhost:1234'
+    specs: 'tests/e2e/*.js',
+    baseUrl: 'http://localhost:1234',
+    framework: 'mocha',
+
+    mochaOpts: {
+        reporter: "mocha",
+        slow: 3000
+    }
 };
