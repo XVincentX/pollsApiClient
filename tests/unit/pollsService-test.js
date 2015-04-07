6a754f839b8f0adfe5a783cd18eb7e8e1606a1f3
Vincenzo Chianese <vincenz.chianese@yahoo.it>
2015-04-07 13:34:02 +0200

    Switch from jasmite to mocha for e2e tests.

--- tests/unit/pollsService-test.js
+++ tests/unit/pollsService-test.js
@@ -2,16 +2,16 @@ import 'angular-mocks'
 import '../../app/main'
 
 describe('PollsService unit tests', function () {
-    var Restangular = null
+  var Restangular = null
 
-    beforeEach(angular.mock.module('pollsClient'));
+  beforeEach(angular.mock.module('pollsClient'));
 
-    beforeEach(angular.mock.inject(function (_Restangular_) {
-        Restangular = _Restangular_;
-    }));
+  beforeEach(angular.mock.inject(function (_Restangular_) {
+    Restangular = _Restangular_;
+  }));
 
-    it('Should touch nasino', function () {
-        console.log(Restangular);
-        assert.isTrue(true);
-    });
+  it('Should touch nasino', function () {
+    console.log(Restangular);
+    assert.isTrue(true);
+  });
 });
