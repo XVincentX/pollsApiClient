93557608c4da56c214e2c6532d9e6e7fad2741fe
XVincentX <vincenz.chianese@yahoo.it>
2015-04-19 23:09:05 +0200

    Added init implementation

--- app/services/pollsService.js
+++ app/services/pollsService.js
@@ -1,6 +1,11 @@
 export default class pollsService {
   constructor(hrRoot) {
-    this.hrRoot = hrRoot
-    this.mainUrl = hrRoot('http://polls.apiblueprint.org').follow()
+    this.mainLink = hrRoot('http://polls.apiblueprint.org').follow({ headers: { Accept: 'application/hal+json' } })
+
+    this.mainLink.$promise.then((hrRootLink)=> {
+        console.info("Fetched hypermedia api from polls")
+        this.hrRootLink = hrRootLink
+    }, (error) => {console.log("Error during hypermedia fetching:" + error)})
+
   }
 }
