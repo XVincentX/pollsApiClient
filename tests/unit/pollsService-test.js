708fdb7e409e462e95692062883b97655691fcc3
XVincentX <vincenz.chianese@yahoo.it>
2015-05-10 12:56:52 +0200

    Get polls tested

--- tests/unit/pollsService-test.js
+++ tests/unit/pollsService-test.js
@@ -4,23 +4,33 @@ import '../../app/main'
 describe('PollsService unit tests', () => {
   var pollsService = null
 
-  beforeEach(angular.mock.module('pollsClient'));
+  beforeEach(angular.mock.module('pollsClient', ($provide) =>{
+    $provide.constant('apiLocation','http://private-anon-68b1c0349-pollshypermedia.apiary-mock.com/')
+  }));
   beforeEach(inject((_pollsService_) => {
     pollsService = _pollsService_;
   }));
 
-  it('should return true', () => {
+  it('Test init', () => {
       assert.isTrue(true)
     })
 
   describe('If I try to get current polls', () => {
+    let pollsData = null
+
     before(() => {
-      pollsService.getPolls()
+      pollsData = pollsService.getPolls()
     })
 
-    it('should return true', () => {
-      assert.isTrue(true)
+    it('should return 1 question', () => {
+      debugger
+      assert.eventually.lengthOf(pollsData, 1)
     })
+
+    it('should return 4 choices', () => {
+      assert.eventually.lengthOf(pollsData, 'choices', 4)
+    })
+
   })
 
 });
