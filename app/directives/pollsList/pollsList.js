b7cd0f6a5d169744bbf1ff6217b53dc7c5d43407
XVincentX <vincenz.chianese@yahoo.it>
2015-05-10 15:22:24 +0200

    Added flex layout to the body

--- app/directives/pollsList/pollsList.js
+++ app/directives/pollsList/pollsList.js
@@ -1,6 +1,8 @@
 import template from './pollsList.template.html!text'
 import controller from './pollsListController'
 
+import './pollsList.css!'
+
 export default class pollsListDirective {
   constructor() {
     this.restrict = 'E'
