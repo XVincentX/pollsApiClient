b49edfdf6349dfa8219a80536e7137d51786ebff
XVincentX <vincenz.chianese@yahoo.it>
2015-05-19 00:10:51 +0200

    Style fixes

--- app/directives/poll/pollController.js
+++ app/directives/poll/pollController.js
@@ -2,20 +2,10 @@ export default class pollController {
   constructor() {
   }
 
-  canVote(choice)
-  {
-    debugger
-    return choice.actions.vote != null
-  }
-
   vote(choice) {
-    if (this.canVote(choice))
+    if (choice.actions.vote != null)
     {
       alert('vote noted')
     }
-    else
-      {
-        alert('you cannot vote this')
-      }
   }
 }
