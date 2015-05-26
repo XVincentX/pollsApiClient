a8425ce436bfc24cf77137aecb67da257732d2f9
XVincentX <vincenz.chianese@yahoo.it>
2015-05-26 23:27:22 +0200

    Added mock address

--- app/main.js
+++ app/main.js
@@ -15,7 +15,8 @@ angular.module('pollsClient', [
     hrRes,
     'isteven-omni-bar'
 ])
-  .constant('apiLocation', 'https://polls.apiblueprint.org/')
+  //.constant('apiLocation', 'https://polls.apiblueprint.org/')
+  .constant('apiLocation', 'http://private-851d-pollshypermedia.apiary-mock.com/')
 
 register('pollsClient')
   .directive('application', applicationDirective)
