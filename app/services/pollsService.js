d902927687cf637b532422313c32bbb26374293b
XVincentX <vincenz.chianese@yahoo.it>
2015-05-28 08:35:59 +0200

    Added global actions and links

--- app/services/pollsService.js
+++ app/services/pollsService.js
@@ -25,15 +25,16 @@ export default class pollsService {
         .then((qLink) => {
         let questions = qLink.$embeddeds('questions')
         if (questions.length == 0)
-          return qLink.$embeddeds('question')
-        return questions
+          return {questions: qLink.$embeddeds('question'), actions: qLink.$forms(), links: qLink.$links()}
+        return {questions: questions, actions: qLink.$forms(), links: qLink.$links()}
       })
     }
 
     let mapPolls = (hyResLinks) =>
     {
+      debugger
       console.info("Received polls informations")
-      return _(hyResLinks).map((element) => {
+      return _(hyResLinks.questions).map((element) => {
         let _choices = _(element.$embeddeds('choices'))
         if (element.$embeddeds('choices').length == 0)
           _choices = _(element.$embeddeds('choice'))
@@ -51,6 +52,8 @@ export default class pollsService {
               }
             }
           }).value(),
+          actions: hyResLinks.actions,
+          links: hyResLinks.links,
           total: _(element.$embeddeds('choices')).reduce((innerTot, choice) => {
               return innerTot + choice.votes
           }, 0)
