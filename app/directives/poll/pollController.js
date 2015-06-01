7f79bb283b61792210a6d8813366ccfafa830e3c
XVincentX <vincenz.chianese@yahoo.it>
2015-06-01 22:12:49 +0200

    Added basic animation to page status

--- app/directives/poll/pollController.js
+++ app/directives/poll/pollController.js
@@ -1,13 +1,21 @@
 export default class pollController {
   constructor(pollsService) {
     this.pollsService = pollsService
+    this.buttonOptions = {
+      success_class:  'success',
+      error_class:    'danger',
+      progress_class: 'progress',
+      idle_class:     'idle',
+      delay:          1500
+    };
   }
 
   vote(choice) {
     if (choice.actions.vote != null)
     {
-      this.pollsService.voteChoice(choice)
-        .then(() => {
+      choice.promise = this.pollsService.voteChoice(choice)
+
+      choice.promise.then(() => {
           console.info(`Voted ${choice.text}`)
           choice.votes++;
           this.poll.total++;
