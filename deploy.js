57133d45862a64ab5b9cdec88a0cd2c109025788
XVincentX <vincenz.chianese@yahoo.it>
2015-06-07 19:56:53 +0200

    Added dlLoader to deploy

--- deploy.js
+++ deploy.js
@@ -3,6 +3,9 @@ var jspm = require('jspm')
 if (process.env.NODE_ENV === 'production')
 {
   jspm.install(true, {})
+    .then(function(){
+      jspm.dlLoader()
+    })
     .then(function() {
       jspm.bundleSFX('app/main', 'build.js', {sourceMaps: false, inject: false, minify: true})
     })
