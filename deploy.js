a9d7553d57be2974c8a2a534f246281580fd2bd2
XVincentX <vincenz.chianese@yahoo.it>
2015-06-08 22:33:25 +0200

    Added informative messages

--- deploy.js
+++ deploy.js
@@ -5,9 +5,11 @@ if (process.env.NODE_ENV === 'production')
   console.log('Executing deploy script')
   jspm.install(true, {})
     .then(function(){
+      console.log("Dl downloading")
       return jspm.dlLoader()
     })
     .then(function() {
+      console.log("Bundle creation")
       return jspm.bundleSFX('app/main', 'build.js', {sourceMaps: false, inject: false, minify: true})
     })
     .then(function(){
