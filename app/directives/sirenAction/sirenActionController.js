7212d1fc31a5f32098fc4749c6d0794b80a654ff
XVincentX <vincenz.chianese@yahoo.it>
2015-06-13 11:24:51 +0200

    Added MVP for add polls

--- app/directives/sirenAction/sirenActionController.js
+++ app/directives/sirenAction/sirenActionController.js
@@ -3,6 +3,15 @@ import _ from 'lodash'
 export default class pollController {
   constructor()
   {
+
+    this.buttonOptions = {
+      success_class:  'success',
+      error_class:    'danger',
+      progress_class: 'progress',
+      idle_class:     'idle',
+      delay:          1500
+    };
+
     this.model = {}
     this.fields = _(this.action.fields).map(field => {
       return {
@@ -16,16 +25,17 @@ export default class pollController {
 
     //Fixme
 
-    this.text = 'baffo'
-    this.choices = ['baffo']
+    this.text = ''
+    this.choices = ['']
   }
 
   createPoll()
   {
     this.action.field("question").value = this.text
     this.action.field("choices").value = this.choices
-    this.action.submit().$promise.then((response) => {
-      debugger
+    this.promise = this.action.submit().$promise
+    this.promise.then((response) => {
+
     })
   }
 }
