e5e6dcebb7f30dd628b7c22555f5541e8a0ae6bd
XVincentX <vincenz.chianese@yahoo.it>
2015-06-14 20:56:31 +0200

    updated loader

--- config.js
+++ config.js
@@ -21,9 +21,9 @@ System.config({
     "angular-hy-res": "npm:angular-hy-res@0.0.22",
     "angular-mocks": "github:angular/bower-angular-mocks@1.4.0",
     "api-check": "npm:api-check@7.3.0",
-    "babel": "npm:babel-core@5.5.7",
-    "babel-runtime": "npm:babel-runtime@5.5.7",
-    "core-js": "npm:core-js@0.9.16",
+    "babel": "npm:babel-core@5.5.8",
+    "babel-runtime": "npm:babel-runtime@5.5.8",
+    "core-js": "npm:core-js@0.9.17",
     "css": "github:systemjs/plugin-css@0.1.12",
     "lodash": "npm:lodash@3.9.3",
     "ng-promise-status": "npm:ng-promise-status@0.1.2",
@@ -110,6 +110,9 @@ System.config({
     "npm:babel-runtime@5.5.7": {
       "process": "github:jspm/nodelibs-process@0.1.1"
     },
+    "npm:babel-runtime@5.5.8": {
+      "process": "github:jspm/nodelibs-process@0.1.1"
+    },
     "npm:bootstrap@3.3.4": {
       "fs": "github:jspm/nodelibs-fs@0.1.2",
       "path": "github:jspm/nodelibs-path@0.1.0",
@@ -140,6 +143,11 @@ System.config({
       "process": "github:jspm/nodelibs-process@0.1.1"
     },
     "npm:core-js@0.9.16": {
+      "fs": "github:jspm/nodelibs-fs@0.1.2",
+      "process": "github:jspm/nodelibs-process@0.1.1",
+      "systemjs-json": "github:systemjs/plugin-json@0.1.0"
+    },
+    "npm:core-js@0.9.17": {
       "fs": "github:jspm/nodelibs-fs@0.1.2",
       "process": "github:jspm/nodelibs-process@0.1.1",
       "systemjs-json": "github:systemjs/plugin-json@0.1.0"
