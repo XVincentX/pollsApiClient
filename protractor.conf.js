69420c72b6f9adabc6bbe027c24d4313efea534c
XVincentX <vincenz.chianese@yahoo.it>
2015-06-16 09:45:42 +0200

    Set up data for saucelabs

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,10 +1,15 @@
-require("./server")
+ci = process.env.CI !== undefined
+
+if (!ci)
+  require("./server")
 
 exports.config = {
-  directConnect: true,
+  directConnect: !ci,
   specs: 'tests/e2e/*.js',
-  baseUrl: 'http://localhost:1234',
+  baseUrl: (ci ? 'http://pollsapiclient.herokuapp.com' : 'http://localhost:1234'),
   framework: 'jasmine2',
+  sauceUser: 'vincenzchianese',
+  sauceKey : 'ebcc4534-bdcc-4ad5-8fd2-31ea2a1b6fce',
   jasmineNodeOpts: {
     showColors: true,
     defaultTimeoutInterval: 30000,
