e07ab3f3ee73837489f8164262c08ef8535834af
XVincentX <vincenz.chianese@yahoo.it>
2015-06-16 23:54:04 +0200

    Added useless (but basic) test

--- tests/e2e/e2e-test.js
+++ tests/e2e/e2e-test.js
@@ -4,8 +4,25 @@ var chaiAsPromised = require('chai-as-promised');
 chai.use(chaiAsPromised);
 var expect = chai.expect;
 
-describe('angularjs homepage todo list', function() {
-  it('should add a todo', function() {
+describe('polls CRUD cycle', function() {
+  describe('If I go to home page', function() {
     browser.get('http://localhost:1234');
+  });
+
+  describe('And I create a new poll', function() {
+    values = ['Cat', 'Dog', 'Elephant', 'Crocodile']
+    element(by.model('ctrl.text')).sendKeys('What is your favourite pet?');
+
+    for (var i = 0; i < values.length; i++)
+    {
+      element.all(by.model('ctrl.choices[$index]')).get(i).sendKeys(values[i]);
+      element.all(by.css('.form-group .choiceLine .btn')).get(i).click();
+    }
+
+    element(by.buttonText('Create')).click();
+  });
+
+  it('Should have a nasino', function(){
+    expect(true).toBe(true);
   });
 });
