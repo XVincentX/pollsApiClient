b73504b211db987623f015625d2a637fe679a365
XVincentX <vincenz.chianese@yahoo.it>
2015-06-16 09:15:21 +0200

    Removed bootstra templates

--- config.js
+++ config.js
@@ -18,7 +18,6 @@ System.config({
     "angular": "npm:angular@1.4.0",
     "angular-animate": "npm:angular-animate@1.4.0",
     "angular-formly": "npm:angular-formly@6.14.0",
-    "angular-formly-templates-bootstrap": "npm:angular-formly-templates-bootstrap@4.3.2",
     "angular-hy-res": "npm:angular-hy-res@0.0.22",
     "angular-mocks": "github:angular/bower-angular-mocks@1.4.0",
     "api-check": "npm:api-check@7.3.0",
@@ -85,14 +84,6 @@ System.config({
       "path": "github:jspm/nodelibs-path@0.1.0",
       "process": "github:jspm/nodelibs-process@0.1.1"
     },
-    "npm:angular-formly-templates-bootstrap@4.3.2": {
-      "angular": "npm:angular@1.4.0",
-      "angular-formly": "npm:angular-formly@6.14.0",
-      "api-check": "npm:api-check@7.3.0",
-      "bootstrap": "npm:bootstrap@3.3.4",
-      "fs": "github:jspm/nodelibs-fs@0.1.2",
-      "path": "github:jspm/nodelibs-path@0.1.0"
-    },
     "npm:angular-formly@6.14.0": {
       "angular": "npm:angular@1.4.0",
       "fs": "github:jspm/nodelibs-fs@0.1.2"
@@ -110,11 +101,6 @@ System.config({
       "util": "npm:util@0.10.3"
     },
     "npm:babel-runtime@5.5.8": {
-      "process": "github:jspm/nodelibs-process@0.1.1"
-    },
-    "npm:bootstrap@3.3.4": {
-      "fs": "github:jspm/nodelibs-fs@0.1.2",
-      "path": "github:jspm/nodelibs-path@0.1.0",
       "process": "github:jspm/nodelibs-process@0.1.1"
     },
     "npm:buffer@3.2.2": {
