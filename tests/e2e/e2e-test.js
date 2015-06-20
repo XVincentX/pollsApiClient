91e761c88a50e53048185cbdbeca93bc3588324a
XVincentX <vincenz.chianese@yahoo.it>
2015-06-20 13:14:33 +0200

    Added return statements

--- tests/e2e/e2e-test.js
+++ tests/e2e/e2e-test.js
@@ -21,11 +21,11 @@ describe('polls CRUD cycle', function() {
         element.all(by.css('.form-group .choiceLine .btn')).get(i).click();
       }
 
-      element(by.buttonText('Create')).click();
+      return element(by.buttonText('Create')).click();
     });
 
     describe('And I get the first element', function(){
-      poll = element.all(by.css('.poll')).get(0);
+      return poll = element.all(by.css('.poll')).get(0);
     })
 
     it('I should find that poll with ' + question + ' as title' , function(){
@@ -35,13 +35,12 @@ describe('polls CRUD cycle', function() {
 
   describe('Delete', function() {
     describe('When then I try to delete that poll', function() {
-      element.all(by.css('.poll')).get(0).element(by.buttonText('delete')).click()
-      var poll = element.all(by.css('.poll')).get(0);
+      return element.all(by.css('.poll')).get(0).element(by.buttonText('delete')).click()
+    })
 
-      it('I should not find that poll with ' + question + ' as title' , function(){
-        expect(poll.element(by.css('h4')).getText()).not.toBe(question)
+    it('I should not find that poll with ' + question + ' as title' , function(){
+        expect(element.all(by.css('.poll')).get(0).element(by.css('h4')).getText()).not.toBe(question)
       });
 
-    })
   })
 });
