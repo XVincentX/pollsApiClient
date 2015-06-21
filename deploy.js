2d217f688f87957a63983f911849aadc08bdc82e
XVincentX <vincenz.chianese@yahoo.it>
2015-06-21 15:44:12 +0200

    Autoprefixer

--- deploy.js
+++ deploy.js
@@ -1,4 +1,7 @@
-var jspm = require('jspm')
+var fs           = require('fs')
+var jspm         = require('jspm')
+var autoprefixer = require('autoprefixer-core');
+var postcss      = require('postcss');
 
 if (process.env.NODE_ENV === 'production')
 {
@@ -12,8 +15,14 @@ if (process.env.NODE_ENV === 'production')
       console.log("Bundle creation")
       return jspm.bundleSFX('app/main', 'build.js', {sourceMaps: false, inject: false, minify: true, mangle: false})
     })
-    .then(function(){
+    .then(function() {
+      var css = fs.readFileSync('./build.css', {encoding: 'utf8' })
+      return postcss([ autoprefixer ]).process(css)
+    })
+    .then(function(result) {
+      fs.writeFileSync('./build.css', result.css)
       console.log('Deploy finished')
       process.exit(0)
-  })
+
+    })
 }
